08a50e5ad9882840d673f7ff4442c4d8fce3e422
Saloniibonde <salonibonde02@gmail.com>
2020-06-05 14:31:54 +0530

    Updated with output

--- cachematrix.R
+++ cachematrix.R
@@ -35,5 +35,15 @@ cacheSolve <- function(x, ...) {
   m
 }
 x<-matrix(1:4,2,2)
+x
 cacheSolve(makeCacheMatrix(x))
 
+#> x
+#[,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> cacheSolve(makeCacheMatrix(x))
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> 
